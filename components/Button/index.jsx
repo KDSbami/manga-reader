7c21068b1b015adaae1428da21abd8d43970af74
sidharthmirch <sidharthmirch@gmail.com>
2021-08-28 22:47:54 +0530

    button component created

---
+++ components/Button/index.jsx
@@ -0,0 +1,14 @@
+import { React, useContext } from 'react';
+import { ThemeContext } from '../Theme';
+
+export default function Button({ onClick, children, title }) {
+    const { theme } = useContext(ThemeContext);
+    return(
+        <button className={`bg-${theme}-200`} onClick={onClick}>
+          {children ? children : 
+          <span className={`${theme}-textColor bg-${theme}-button`}>
+              {title}
+          </span>}
+        </button>
+    )
+}
