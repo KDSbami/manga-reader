2f0d59f7fbe089cafd809557eb4c2fcec91c1341
kdsbami <karamdeep.singh11@gmail.com>
2022-01-29 02:28:59 +0530

    added support for icons

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,22 @@
-import { React, useState } from "react";
+import { React, useState, useContext, useEffect } from "react";
 import { useRouter } from "next/router";
 import Container from "../src/components/Container";
 import InputField from "../src/components/InputField";
+import { ThemeContext } from "../src/components/Theme";
 import { fetchData, fetchDataWithAuth } from "../src/services/apiServices";
 
 const Home = () => {
   const router = useRouter();
   const [query, setQuery] = useState("");
+  const [searchIcon, setSearchIcon] = useState("");
+  let { theme, setTheme } = useContext(ThemeContext);
+
+  useEffect(()=>{
+    setSearchIcon(
+      theme==="dark"
+      ?"https://cdn.discordapp.com/attachments/936709028657127477/936709131719565332/searchDM.png"
+      :"https://cdn.discordapp.com/attachments/936709028657127477/936709132046700554/searchLM.png")
+  },[theme])
 
   const onQueryChange = (e) => {
     setQuery(e.target.value);
@@ -20,6 +30,9 @@ const Home = () => {
   return (
     <Container>
       <div className="grid grid-rows-5 h-full">
+        <div className="row-start-2 mx-auto flex items-end text-5xl">
+          M.
+        </div>
         <div className="row-start-3 pt-2">
           <div className="max-w-640px m-auto p-4">
             <InputField
@@ -27,6 +40,9 @@ const Home = () => {
               inputCallback={onQueryChange}
               handleKeyDown={onKeyDown}
               value={query}
+              // advised not to use until the inputField hover is fixed for divs
+              // icon={searchIcon} 
+
             />
           </div>
         </div>  
