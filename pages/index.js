249e28dcd5339552835cdcee75899dbad3e8dc3f
kdsbami <karamdeep.singh11@gmail.com>
2022-01-28 19:09:01 +0530

    Added basic styles and layout
    Co-authored-by: sidharth <sidharthmirch@users.noreply.github.com>

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,39 @@
-import { React, useEffect } from "react";
+import { React, useState } from "react";
+import { useRouter } from "next/router";
 import Container from "../src/components/Container";
+import InputField from "../src/components/InputField";
 import { fetchData, fetchDataWithAuth } from "../src/services/apiServices";
 
 const Home = () => {
-  useEffect(() => {
-    fetchDataWithAuth("api.mangadex.org", "GET_MANGA_LIST", {
-      offset: "5",
-      limit: "25",
-    }).then((data) => {
-      console.log(data);
-    });
-  }, []);
+  const router = useRouter();
+  const [query, setQuery] = useState("");
 
-  return <Container>Manga Reader</Container>;
+  const onQueryChange = (e) => {
+    setQuery(e.target.value);
+  }
+
+  const onKeyDown = (e) => {
+    if (e.key !== "Enter") return;
+    router.push(`/search?q=${query}`);
+  }
+
+  return (
+    <Container>
+      <div className="grid grid-rows-5 h-full">
+        <div className="row-start-3 pt-2">
+          <div className="max-w-640px m-auto">
+            <InputField
+              placeholder={""}
+              inputCallback={onQueryChange}
+              handleKeyDown={onKeyDown}
+              value={query}
+            />
+          </div>
+        </div>  
+      </div>
+      
+    </Container>
+  );
 };
 
 export default Home;
--- src/components/InputField/index.js
+++ src/components/InputField/index.js
@@ -6,18 +6,18 @@ const InputField = ({ placeholder, value, inputCallback, handleKeyDown }) => {
   let { theme, setTheme } = useContext(ThemeContext);
   let darkModeStyle =
     theme === "dark"
-      ? "transition ease-in-out duration-150 bg-foreground hover:bg-foreground-shading"
+      ? "transition ease-in-out duration-150 bg-background hover:bg-foreground-shading"
       : "bg-foreground";
   let shadow =
     "border border-foreground-25 hover:border-foreground transition ease-in-out duration-150 shadow-xl hover:shadow-md";
   return (
     <div
-      className={`flex flex-row align-center h-44px rounded-full w-full pl-8 bg-foreground overflow-hidden ${darkModeStyle} ${shadow}`}
+      className={`flex flex-row align-center h-44px rounded-full w-full pl-8 overflow-hidden ${darkModeStyle} ${shadow}`}
     >
       <input
         placeholder={placeholder}
         value={value}
-        className={`placeholder-foreground-25 hover:placeholder-accent1 outline-none bg-foreground text-foreground-accent text-xl w-full ${darkModeStyle}`}
+        className={`placeholder-foreground-25 hover:placeholder-accent1 outline-none bg-background text-foreground-accent text-xl w-full ${darkModeStyle}`}
         onChange={inputCallback}
         onKeyDown={handleKeyDown}
       />
