10a47fba64a14689d12babc608808c7d05f71ba3
sidharthmirch <32238869+sidharthmirch@users.noreply.github.com>
2022-02-02 21:03:55 +0530

    fetch 512x.jpg

--- src/components/Showcase/index.js
+++ src/components/Showcase/index.js
@@ -1,11 +1,11 @@
 const Showcase = ({ coverUrl, mangaTitle }) => {
   return (
-    <div className="flex overflow-hidden rounded-xl">
+    <div className="flex overflow-hidden w-full rounded-xl">
       <div className="h-min self-center">
         <img
           crossOrigin="anonymous"
           src={coverUrl}
-          width="500"
+          width="384"
           className="filter grayscale-1 transition ease-in-out duration-300 grayscale-25 hover:grayscale-0"
           alt={mangaTitle}
         />
