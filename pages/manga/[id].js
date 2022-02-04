bc4157fc94760c2ebd747978f90813d1de208911
sidharthmirch <32238869+sidharthmirch@users.noreply.github.com>
2022-02-04 23:38:42 +0530

    retrival of cover id's done...

--- pages/manga/[id].js
+++ pages/manga/[id].js
@@ -88,7 +88,7 @@ const Manga = () => {
                   animate={true}
                   title={`read ${title}`}
                   onClick={() => {
-                    alert(`reading`);
+                    router.push(`read?id=${router.query.id}`);
                   }}
                 />
               </div>
